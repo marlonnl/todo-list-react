3f80bf34de8c7ffbe84c90e855449e2aa9428872
Marlonn Locatelli <marlonn@gmail.com>
2024-09-30 17:39:49 -0300

    Instalação do pacote react-router-dom e criação da home e preparação da página de cadastro de tarefas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
-import GlobalStyle, { Container } from './styles'
-import Sidebar from './containers/Sidebar'
-import Tasklist from './containers/Tasklist'
 import { Provider } from 'react-redux'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import GlobalStyle, { Container } from './styles'
 
 import store from './store'
+import Home from './pages/Home'
+
+const rota = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/new',
+    element: <p>nova tarefa</p>
+  }
+])
 
 function App() {
   return (
     <Provider store={store}>
       <GlobalStyle />
       <Container>
-        <Sidebar />
-        <Tasklist />
+        <RouterProvider router={rota} />
       </Container>
     </Provider>
   )
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import Sidebar from '../../containers/Sidebar'
+import Tasklist from '../../containers/Tasklist'
+
+const Home = () => (
+  <>
+    <Sidebar />
+    <Tasklist />
+  </>
+)
+
+export default Home
